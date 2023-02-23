4ff4e4737aeea15f9d99e8cc5c032bd3271f8256
itaylotan <itay235711@gmail.com>
2023-02-23 21:11:12 +0200

    small refactor

--- monet_cyclegan_tutorial.py
+++ monet_cyclegan_tutorial.py
@@ -590,13 +590,13 @@ class CycleGan(keras.Model):
             photo_generator,
             monet_discriminator,
             photo_discriminator,
-            lambda_cycle=10,
+            lambda_cycle,
     ):
         super(CycleGan, self).__init__()
-        self.m_gen = monet_generator
-        self.p_gen = photo_generator
-        self.m_disc = monet_discriminator
-        self.p_disc = photo_discriminator
+        self._monet_generator = monet_generator
+        self._photo_generator = photo_generator
+        self._monet_discriminator = monet_discriminator
+        self._photo_discriminator = photo_discriminator
         self.lambda_cycle = lambda_cycle
 
     def compile(
@@ -619,82 +619,67 @@ class CycleGan(keras.Model):
         self.disc_loss_fn = disc_loss_fn
         self.cycle_loss_fn = cycle_loss_fn
         self.identity_loss_fn = identity_loss_fn
-        self._my_hack_history = []
 
     def train_step(self, batch_data):
-        real_monet, real_photo = batch_data
+        monet_img, photo_img = batch_data
 
         with tf.GradientTape(persistent=True) as tape:
-            # photo to monet back to photo
-            fake_monet = self.m_gen(real_photo, training=True)
-            cycled_photo = self.p_gen(fake_monet, training=True)
-
-            # monet to photo back to monet
-            fake_photo = self.p_gen(real_monet, training=True)
-            cycled_monet = self.m_gen(fake_photo, training=True)
-
-            # generating itself
-            same_monet = self.m_gen(real_monet, training=True)
-            same_photo = self.p_gen(real_photo, training=True)
-
-            # discriminator used to check, inputing real images
-            disc_real_monet = self.m_disc(real_monet, training=True)
-            disc_real_photo = self.p_disc(real_photo, training=True)
-
-            # discriminator used to check, inputing fake images
-            disc_fake_monet = self.m_disc(fake_monet, training=True)
-            disc_fake_photo = self.p_disc(fake_photo, training=True)
-
-            # evaluates generator loss
-            monet_gen_loss = self.gen_loss_fn(disc_fake_monet)
-            photo_gen_loss = self.gen_loss_fn(disc_fake_photo)
-
-            # evaluates total cycle consistency loss
-            total_cycle_loss = self.cycle_loss_fn(real_monet, cycled_monet, self.lambda_cycle) + self.cycle_loss_fn(
-                real_photo, cycled_photo, self.lambda_cycle)
-
-            # evaluates total generator loss
-            total_monet_gen_loss = monet_gen_loss + total_cycle_loss + self.identity_loss_fn(real_monet, same_monet,
-                                                                                             self.lambda_cycle)
-            total_photo_gen_loss = photo_gen_loss + total_cycle_loss + self.identity_loss_fn(real_photo, same_photo,
-                                                                                             self.lambda_cycle)
-
-            # evaluates discriminator loss
-            monet_disc_loss = self.disc_loss_fn(disc_real_monet, disc_fake_monet)
-            photo_disc_loss = self.disc_loss_fn(disc_real_photo, disc_fake_photo)
-
-        # Calculate the gradients for generator and discriminator
-        monet_generator_gradients = tape.gradient(total_monet_gen_loss,
-                                                  self.m_gen.trainable_variables)
-        photo_generator_gradients = tape.gradient(total_photo_gen_loss,
-                                                  self.p_gen.trainable_variables)
-
-        monet_discriminator_gradients = tape.gradient(monet_disc_loss,
-                                                      self.m_disc.trainable_variables)
-        photo_discriminator_gradients = tape.gradient(photo_disc_loss,
-                                                      self.p_disc.trainable_variables)
-
-        # Apply the gradients to the optimizer
-        self.m_gen_optimizer.apply_gradients(zip(monet_generator_gradients,
-                                                 self.m_gen.trainable_variables))
-
-        self.p_gen_optimizer.apply_gradients(zip(photo_generator_gradients,
-                                                 self.p_gen.trainable_variables))
-
-        self.m_disc_optimizer.apply_gradients(zip(monet_discriminator_gradients,
-                                                  self.m_disc.trainable_variables))
-
-        self.p_disc_optimizer.apply_gradients(zip(photo_discriminator_gradients,
-                                                  self.p_disc.trainable_variables))
-
-        rett = {
-            "monet_gen_loss": total_monet_gen_loss,
-            "photo_gen_loss": total_photo_gen_loss,
+            monet_disc_loss, photo_disc_loss, monet_gen_loss, photo_gen_loss = \
+                self._calc_internal_models_losses(monet_img, photo_img)
+        self._apply_internal_models_gradients(
+            monet_disc_loss, photo_disc_loss, photo_gen_loss, monet_gen_loss, tape
+        )
+        ret_losses = {
+            "monet_gen_loss": monet_gen_loss,
+            "photo_gen_loss": photo_gen_loss,
             "monet_disc_loss": monet_disc_loss,
             "photo_disc_loss": photo_disc_loss
         }
-        self._my_hack_history.append(rett)
-        return rett
+        return ret_losses
+
+    def _calc_internal_models_losses(self, real_monet, real_photo):
+        fake_monet = self._monet_generator(real_photo, training=True)
+        cycled_photo = self._photo_generator(fake_monet, training=True)
+
+        fake_photo = self._photo_generator(real_monet, training=True)
+        cycled_monet = self._monet_generator(fake_photo, training=True)
+
+        same_monet = self._monet_generator(real_monet, training=True)
+        same_photo = self._photo_generator(real_photo, training=True)
+
+        disc_real_monet = self._monet_discriminator(real_monet, training=True)
+        disc_real_photo = self._photo_discriminator(real_photo, training=True)
+
+        disc_fake_monet = self._monet_discriminator(fake_monet, training=True)
+        disc_fake_photo = self._photo_discriminator(fake_photo, training=True)
+
+        monet_gen_loss = self.gen_loss_fn(disc_fake_monet)
+        photo_gen_loss = self.gen_loss_fn(disc_fake_photo)
+
+        total_cycle_loss = self.cycle_loss_fn(real_monet, cycled_monet, self.lambda_cycle) + \
+                           self.cycle_loss_fn(real_photo, cycled_photo, self.lambda_cycle)
+
+        final_monet_gen_loss = monet_gen_loss + total_cycle_loss + \
+                               self.identity_loss_fn(real_monet, same_monet, self.lambda_cycle)
+        final_photo_gen_loss = photo_gen_loss + total_cycle_loss + \
+                               self.identity_loss_fn(real_photo, same_photo, self.lambda_cycle)
+
+        final_monet_disc_loss = self.disc_loss_fn(disc_real_monet, disc_fake_monet)
+        final_photo_disc_loss = self.disc_loss_fn(disc_real_photo, disc_fake_photo)
+        return final_monet_disc_loss, final_photo_disc_loss, final_monet_gen_loss, final_photo_gen_loss
+
+    def _apply_internal_models_gradients(
+            self, monet_disc_loss, photo_disc_loss, photo_gen_loss, monet_gen_loss, tape
+    ):
+        monet_generator_gradients = tape.gradient(monet_gen_loss, self._monet_generator.trainable_variables)
+        photo_generator_gradients = tape.gradient(photo_gen_loss, self._photo_generator.trainable_variables)
+        monet_discriminator_gradients = tape.gradient(monet_disc_loss, self._monet_discriminator.trainable_variables)
+        photo_discriminator_gradients = tape.gradient(photo_disc_loss, self._photo_discriminator.trainable_variables)
+
+        self.m_gen_optimizer.apply_gradients(zip(monet_generator_gradients, self._monet_generator.trainable_variables))
+        self.p_gen_optimizer.apply_gradients(zip(photo_generator_gradients, self._photo_generator.trainable_variables))
+        self.m_disc_optimizer.apply_gradients(zip(monet_discriminator_gradients, self._monet_discriminator.trainable_variables))
+        self.p_disc_optimizer.apply_gradients(zip(photo_discriminator_gradients, self._photo_discriminator.trainable_variables))
 
 
 def _build_losses():
@@ -747,7 +732,7 @@ def _build_cycle_gan_model(train_settings):
     photo_discriminator_optimizer = optimizer_builder()
 
     cycle_gan_model = CycleGan(
-        monet_generator, photo_generator, monet_discriminator, photo_discriminator
+        monet_generator, photo_generator, monet_discriminator, photo_discriminator, lambda_cycle=10
     )
     cycle_gan_model.compile(
         m_gen_optimizer=monet_generator_optimizer,
